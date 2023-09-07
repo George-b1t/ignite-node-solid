c86e4da2a63a2572ed0c06634c536faa614b5de4
George <george.soares509@gmail.com>
2023-09-07 20:56:25 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { app } from './app'
+import { env } from './env'
+
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: env.PORT,
+  },
+  (err) => {
+    if (err) throw err
+    console.log(`🚀 HTTP Server Running!`)
+  },
+)
